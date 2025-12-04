11624ad61e4292ac3e571dd05cdd82a1b378d23a
A-545 <potetomaru1128mm@gmail.com>
2025-12-04 13:46:37 +0900

    test通って

--- test/test.bash
+++ test/test.bash
@@ -6,6 +6,7 @@ dir=~
 [ "$1" != "" ] && dir="$1"   #引数があったら、そちらをホームに変える。
 
 cd $dir/ros2_ws
+export COLCON_PYTHON_EXECUTABLE=/usr/bin/python3
 colcon build
 source $dir/.bashrc
 timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
