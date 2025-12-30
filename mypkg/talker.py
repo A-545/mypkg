21f65c19234a5f6f2fec92be0ec0ebd1575d9f54
A-545 <potetomaru1128mm@gmail.com>
2025-12-30 23:55:29 +0900

    ポモロードタイマー

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -2,18 +2,20 @@ import rclpy #ROS 2のクライアントのためのライブラリ
 from rclpy.node import Node      #ノードを実装するためのNodeクラス（クラスは第10回で）
 from std_msgs.msg import Int16   #通信の型
 
+class Talker():
+    def __init__(self): 
+        self.pub = node.create_publisher(Int16, "countup", 10) 
+        self.n = 0 #カウント用変数
+
 rclpy.init()
 node = Node("talker")            #ノード作成（nodeという「オブジェクト」を作成）
-pub = node.create_publisher(Int16, "countup", 10)   #パブリッシャのオブジェクト作成
-n = 0 #カウント用変数
+talker = Talker()
+
 def cb():          #20行目で定期実行されるコールバック関数
-    global n       #関数を抜けてもnがリセットされないようにしている
     msg = Int16()  #メッセージの「オブジェクト」
-    msg.data = n
-    pub.publish(msg)        #pubの持つpublishでメッセージ送信
-    n += 1
+    msg.data = talker.n
+    talker.pub.publish(msg)        #pubの持つpublishでメッセージ送信
+    talker.n += 1
 
-
-def main():
-    node.create_timer(0.5, cb)  #タイマー設定
-    rclpy.spin(node)            #実行（無限ループ）
+node.create_timer(0.5, cb)  #タイマー設定
+rclpy.spin(node)            #実行（無限ループ）
